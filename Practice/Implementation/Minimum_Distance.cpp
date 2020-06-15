1976481321467cfc7b26a0f1cf621882943fd1d5
DreadArceus <66964119+DreadArceus@users.noreply.github.com>
2020-06-16 01:14:00 +0530

    Create Minimum_Distance.cpp

---
+++ Practice/Implementation/Minimum_Distance.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the minimumDistances function below.
+int minimumDistances(vector<int> a) {
+    int minDis = 1000;
+    for(int x = 0; x < a.size(); x++)
+    {
+        for(int y = x + 1; y < a.size(); y++)
+        {
+            if(a[x] == a[y] && abs(y - x) < minDis)
+            {
+                minDis = abs(y - x);
+            }
+        }
+    }
+    if(minDis == 1000)
+    {
+        return -1;
+    }
+    return minDis;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split_string(a_temp_temp);
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = minimumDistances(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
