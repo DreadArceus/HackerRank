d82b6262ca733dead8c3ea8b98c5664569fbab7b
DreadArceus <66964119+DreadArceus@users.noreply.github.com>
2020-06-18 23:47:06 +0530

    Create The_Love-Letter_Mystery.cpp

---
+++ Practice/Strings/The_Love-Letter_Mystery.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the theLoveLetterMystery function below.
+int theLoveLetterMystery(string s) {
+    int oCount = 0;
+    for(int x = 0; x < s.length(); x++)
+    {
+        oCount += abs(s.at(x) - s.at(s.length() - 1 - x));
+    }
+    oCount /= 2;
+    return oCount;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int q;
+    cin >> q;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = theLoveLetterMystery(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
