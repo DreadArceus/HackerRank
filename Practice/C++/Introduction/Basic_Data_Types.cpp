36307da570063a314bb6ec77f929a399c5952d9a
DreadArceus <66964119+DreadArceus@users.noreply.github.com>
2020-06-19 01:00:48 +0530

    Create Basic_Data_Types.cpp

---
+++ Practice/C++/Introduction/Basic_Data_Types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long l;
+    char c;
+    float f;
+    double d;
+    scanf("%d %ld %c %f %lf", &i, &l, &c, &f, &d);
+    printf("%d", i);
+    cout << endl;
+    printf("%ld", l);
+    cout << endl;a
+    printf("%c", c);
+    cout << endl;
+    printf("%f", f);
+    cout << endl;
+    printf("%lf", d);
+    cout << endl;
+    return 0;
+}
