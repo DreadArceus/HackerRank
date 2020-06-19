0cbd4977d9684066d1e9577829f942e7bd26a48e
DreadArceus <66964119+DreadArceus@users.noreply.github.com>
2020-06-19 16:03:44 +0530

    Update StringStream.cpp

--- Practice/C++/Strings/StringStream.cpp
+++ Practice/C++/Strings/StringStream.cpp
@@ -5,10 +5,18 @@ using namespace std;
 
 vector<int> parseInts(string str) {
 	// Complete this function
+    int l = 1;
+    for(int x = 0; x < str.length()-1; x++)
+    {
+        if(str.at(x) == ',')
+        {
+            l++;
+        }
+    }
     stringstream ss;
     ss << str;
     char d;
-    vector<int> ans(str.length()/2);
+    vector<int> ans(l);
     for(int x = 0; x < ans.size(); x++)
     {
         ss >> ans[x] >> d;
@@ -21,14 +29,7 @@ int main() {
     cin >> str;
     vector<int> integers = parseInts(str);
     for(int i = 0; i < integers.size(); i++) {
-        if(integers[i] != 0 || integers[i+1] != 0)
-        {
-            cout << integers[i] << "\n";
-        }
-        else
-        {
-            break;
-        }
+        cout << integers[i] << "\n";
     }
     
     return 0;
